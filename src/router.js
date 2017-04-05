ffbb42b9c0483f5a866eb367829aec65262c5961
trzl47 <brownty47@gmail.com>
2017-04-05 00:28:05 -0500

    added Header to routing

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Components
 import Home from './containers/Home.js';
+import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import NotFound from './components/NotFound';
 
@@ -11,6 +12,7 @@ import NotFound from './components/NotFound';
 const routes = (
   <Router>
 		<div>
+			<Header />
 			<Switch>
 				<Route exact={true} pattern="/" component={Home} />
 				<Route path="*" component={NotFound} />
