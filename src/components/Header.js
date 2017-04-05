56c4e8a4f46d7d667b89cb069cfb9423d04e3bae
trzl47 <brownty47@gmail.com>
2017-04-04 22:09:01 -0500

    changing "home" nav link to be the logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,10 @@ import React from 'react';
 const Header = () => {
 		return (
 			<header>
-			<nav class="navbar navbar-default navbar-fixed-top">
+			<nav>
 				<div class="container-fluid" role="presentation">
-					<img class="navbar-brand collapse navbar-collapse" src={require("./assets/logo.png")} alt="Adhoc logo" />
+					<img class="navbar-brand collapse navbar-collapse" src={require("./assets/logo.png")} alt="Adhoc logo"><a href="#"></a></img>
 					<ul class="nav navbar-nav navbar-right" role="presentation">
-						<li><a href="#">Home</a></li>
 						<li class="active"><a href="#">About us</a></li>
 						<li><a href="#">Our team</a></li>
 						<li><a href="#">Contact us</a></li>
