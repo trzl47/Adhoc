00d34082911e70e3e87397dba4e8ffbc5df74740
trzl47 <brownty47@gmail.com>
2017-04-04 23:47:00 -0500

    added Team component
    - renders TeamMembers mapped from teammembers object

---
+++ src/components/Team/teammembers.js
@@ -0,0 +1,31 @@
+let count = 0;
+const TeamMembers = {
+  "Home":[
+    {
+			name: 'Brian Murphy',
+			title: 'CEO',
+			img: require('./assets/brian.png'),
+			id: count++
+    },
+		{
+			name: 'Andy Cox',
+			title: 'Co-Founder',
+			img: require('./assets/andy.png'),
+			id: count++
+    },
+		{
+			name: 'Angela Woronick',
+			title: 'CFO',
+			img: require('./assets/angela.png'),
+			id: count++
+    },
+		{
+			name: 'Amie Shadlu',
+			title: 'Angel Investor',
+			img: require('./assets/amie.png'),
+			id: count++
+    },
+  ],
+};
+
+export default TeamMembers;
---
+++ src/containers/Team.js
@@ -0,0 +1,58 @@
+// Libs
+import React, { Component } from 'react';
+// Components
+import TeamMember from '../components/TeamMember.js';
+import teammembers from '../components/teammembers.js';
+
+const renderTeam = (members, length) => {
+	const team = members.map((member) => {
+		return (
+			<div className="col-xs-12 col-sm-3 member"
+					key={member.id}>
+				<TeamMember
+					title={member.title}
+					img={member.img}
+					name={member.name} />
+			</div>
+		)
+	});
+	const chunkMembers = (arr, size) => {
+		let rows = [];
+		for (let i = 0; i < arr.length; i+=size) {
+			rows.push(arr.slice(i,i+size));
+		}
+		return rows;
+	};
+	const renderRows = () => {
+		return (
+			chunkMembers(team, 4).map((row) => {
+				return (
+					<div className="row">
+						{ row }
+					</div>
+				)
+			})
+		)
+	};
+	return (
+		renderRows()
+	);
+};
+
+class Team extends Component {
+	render() {
+		const membersContent = teammembers.Home;
+		return(
+			<div className='team'>
+				<div className="container-fluid team">
+					<div className="row">
+						<h2 className="col-xs-12">Meet the team</h2>
+					</div>
+					{ renderTeam(membersContent, membersContent.length) }
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Team;
