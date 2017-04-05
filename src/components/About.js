537ca3f14500acb5950fa94e7431cd76672f0096
trzl47 <brownty47@gmail.com>
2017-04-04 22:37:43 -0500

    added About component

---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+// Libs
+import React, { Component } from 'react';
+// Components
+
+export default class About extends Component {
+	render() {
+		return(
+				<div className='about'>
+					<div className="container header-line" role="presentation">
+						<div className="row" role="presentation">
+							<div className="col-xs-12 header" role="presentation">
+								<h2>Why Adhoc?</h2>
+							</div>
+						</div>
+						<div className="row" role="presentation">
+							<div className="col-xs-12 col-sm-4 image-container" role="presentation">
+								<img src={require("./assets/experienced.png")} alt="We are experienced" />
+							</div>
+							<div className="col-xs-12 col-sm-4 image-container" role="presentation">
+								<img src={require("./assets/fun.png")} alt="We are fun" />
+							</div>
+							<div className="col-xs-12 col-sm-4 image-container" role="presentation">
+								<img src={require("./assets/smart.png")} alt="We are smart" />
+							</div>
+						</div>
+					</div>
+				</div>
+		);
+	}
+}
