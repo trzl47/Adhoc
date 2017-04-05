971aee71f7549b1e944830b220da435e390638e4
trzl47 <brownty47@gmail.com>
2017-04-05 00:27:50 -0500

    bringin' it Home
    Home page component renders Jumbotron, QuoteBanner, About, Team, and
    Clients components

---
+++ src/containers/Home.js
@@ -0,0 +1,22 @@
+// Libs
+import React, { Component } from 'react';
+// Components
+import Jumbotron from './Jumbotron.js';
+import QuoteBanner from './QuoteBanner.js';
+import About from '../components/About.js';
+import Team from './Team.js';
+import Clients from './Clients.js';
+
+export default class Home extends Component {
+	render() {
+		return(
+			<div className='adhoc'>
+				<Jumbotron />
+				<QuoteBanner />
+				<About />
+				<Team />
+				<Clients />
+			</div>
+		);
+	}
+}
