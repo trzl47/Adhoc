49bee9ecf0265ee06822d618d1cf9319cfe191a0
trzl47 <brownty47@gmail.com>
2017-11-27 00:49:36 -0600

    initial docker dev env setup

---
+++ services/web/src/app/Main.js
@@ -0,0 +1,26 @@
+//Libs
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+// Components
+import Home from './containers/Home.js';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import NotFound from '../app/NotFound';
+
+class Main extends Component {
+	render() {
+		return (
+			<div>
+				<Header />
+				<Switch>
+					<Route exact={true} pattern="/" component={Home} />
+					<Route path="*" component={NotFound} />
+				</Switch>
+				<Footer />
+			</div>
+		);
+	}
+}
+
+export default Main;
