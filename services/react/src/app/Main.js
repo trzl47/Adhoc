84f14dc2726793e384aad704b09a1d0ba9976d59
trzl <brownty47@gmail.com>
2018-06-20 23:28:27 +0000

    added footer component

--- services/react/src/app/Main.js
+++ services/react/src/app/Main.js
@@ -19,7 +19,7 @@ class Main extends Component {
 					<Route exact={true} pattern="/" component={Home} />
 					<Route path="*" component={NotFound} />
 				</Switch>
-				{/* <Footer /> */}
+				<Footer />
 			</div>
 		);
 	}
