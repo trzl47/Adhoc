84495c7dc6104b33ec5483a7b2ff768bc2b99bd9
trzl <brownty47@gmail.com>
2018-06-21 07:33:28 +0000

    updating build script to correct project name

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ docker-compose -f docker-compose-build.yml up --build -d && \
 docker system prune --volumes && \
 mkdir services/adhoc-nginx/build && \
 cp -r services/react/build/client services/adhoc-nginx/build && \
-docker build -t trzl/portfolio:pyt-react.1 -f ./services/adhoc-nginx/Dockerfile.prod ./services/adhoc-nginx && \
-docker push trzl/portfolio:pyt-react.1 && \
-docker build -t trzl/portfolio:pyt-proxy.1 -f ./services/reverseproxy/Dockerfile ./services/reverseproxy && \
-docker push trzl/portfolio:pyt-proxy.1 && \
+docker build -t trzl/portfolio:adhoc-react.1 -f ./services/adhoc-nginx/Dockerfile.prod ./services/adhoc-nginx && \
+docker push trzl/portfolio:adhoc-react.1 && \
+docker build -t trzl/portfolio:adhoc-proxy.1 -f ./services/reverseproxy/Dockerfile ./services/reverseproxy && \
+docker push trzl/portfolio:adhoc-proxy.1 && \
 docker-compose down && \
 docker system prune --volumes && \
 sudo rm -rf services/adhoc-nginx/build && \
